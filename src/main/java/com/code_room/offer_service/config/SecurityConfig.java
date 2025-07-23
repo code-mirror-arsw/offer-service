7d58e59e83f2167ea07579889a1d8628e256fb34
Coronado <retrollaso@gmail.com>
2025-07-22 21:06:11 -0500

    deploy v3

--- src/main/java/com/code_room/offer_service/config/SecurityConfig.java
+++ src/main/java/com/code_room/offer_service/config/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173","https://victorious-water-0ec758310.2.azurestaticapps.net")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
